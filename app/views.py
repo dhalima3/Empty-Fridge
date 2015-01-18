b6d65782fd76579ca78b98150aec991a650c8472
Daryl Halima <dhalima@gatech.edu>
2015-01-17 19:54:38 -0500

    Front end updates

--- app/views.py
+++ app/views.py
@@ -1,6 +1,21 @@
+from flask import render_template
 from app import app
 
 @app.route('/')
 @app.route('/index')
 def index():
-	    return "Hello, World!"
+    user = {'nickname': 'Miguel'}  # fake user
+    recipes = [  # fake array of recipes
+        { 
+            'name': 'BBQ Chicken', 
+            'ingredients': ['salt',' pepper',' chicken'] 
+        },
+        { 
+            'name': 'Goldfish', 
+            'ingredients': ['salt','pepper'] 
+        }
+    ]
+    return render_template("index.html",
+                           title='Home',
+                           user=user,
+                           recipes=recipes)
