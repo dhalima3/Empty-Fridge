94da4e8c8626fff9c17d4b2948acec1ea3a46916
Daryl Halima <dhalima@gatech.edu>
2015-01-18 01:23:45 -0500

    Linked up backend with frontend

--- FoodRecommendation.py
+++ FoodRecommendation.py
@@ -25,43 +25,43 @@ def removeIngredient(ingredients):
         ingredients_list.pop(0)
         return ','.join(ingredients_list)
 
-@app.route("/user/<string:ingredients>")
-@app.route("/user/<string:ingredients>/")
+@app.route("/user/<ingredients>")
+# @app.route("/user/<string:ingredients>/")
 def getRecipe(ingredients):
-    if (ingredients == ""):
-        return str(BASE_RESULT)
-    searchRequest = BASE_SEARCH + API_KEY
-    ingredients = "&q=" + ingredients
-    searchRequest = searchRequest + ingredients
-    try:
-        content = urllib2.urlopen(searchRequest).read()
-        contentDict = ast.literal_eval(content)
-        count = contentDict["count"]
+    # if (ingredients == ""):
+    #     return str(BASE_RESULT)
+    # searchRequest = BASE_SEARCH + API_KEY
+    # ingredients = "&q=" + ingredients
+    # searchRequest = searchRequest + ingredients
+    # try:
+    #     content = urllib2.urlopen(searchRequest).read()
+    #     contentDict = ast.literal_eval(content)
+    #     count = contentDict["count"]
 
-        while ((count == 0) and (numIngredients(ingredients) > 1)):
-            ingredients = removeIngredient(ingredients)
-            content = urllib2.urlopen(BASE_SEARCH + API_KEY + "&q=" + ingredients).read()
-            contentDict = ast.literal_eval(content)
-            count = contentDict["count"]
+    #     while ((count == 0) and (numIngredients(ingredients) > 1)):
+    #         ingredients = removeIngredient(ingredients)
+    #         content = urllib2.urlopen(BASE_SEARCH + API_KEY + "&q=" + ingredients).read()
+    #         contentDict = ast.literal_eval(content)
+    #         count = contentDict["count"]
 
-        if (count == 0):
-            return str(BASE_RESULT)
-        else:
-            finalDict = contentDict["recipes"][0]
-            searchGet = BASE_GET + API_KEY + "&rId=" + finalDict["recipe_id"]
-            finalContentDictStr = urllib2.urlopen(searchGet).read()
-            finalContentDict = ast.literal_eval(finalContentDictStr)
-            completeDict = {"Name" : "",
-                            "URL" : "",
-                            "Ingredients" : "",
-                            "Image_Url" : ""}
-            completeDict["Name"] = finalContentDict["recipe"]["title"]
-            completeDict["URL"] = finalContentDict["recipe"]["source_url"]
-            completeDict["Ingredients"] = str(finalContentDict["recipe"]["ingredients"])
-            completeDict["Image_Url"] = finalContentDict["recipe"]["image_url"]
-            return str(completeDict)
-    except:
-        return str(BASE_RESULT)
-
+    #     if (count == 0):
+    #         return str(BASE_RESULT)
+    #     else:
+    #         finalDict = contentDict["recipes"][0]
+    #         searchGet = BASE_GET + API_KEY + "&rId=" + finalDict["recipe_id"]
+    #         finalContentDictStr = urllib2.urlopen(searchGet).read()
+    #         finalContentDict = ast.literal_eval(finalContentDictStr)
+    #         completeDict = {"Name" : "",
+    #                         "URL" : "",
+    #                         "Ingredients" : "",
+    #                         "Image_Url" : ""}
+    #         completeDict["Name"] = finalContentDict["recipe"]["title"]
+    #         completeDict["URL"] = finalContentDict["recipe"]["source_url"]
+    #         completeDict["Ingredients"] = str(finalContentDict["recipe"]["ingredients"])
+    #         completeDict["Image_Url"] = finalContentDict["recipe"]["image_url"]
+    #         return str(completeDict)
+    # except:
+    #     return str(BASE_RESULT)
+    return 'hi'
 if __name__ == "__main__":
     app.run()
--- app/views.py
+++ app/views.py
@@ -1,5 +1,6 @@
 from flask import render_template
 from app import app
+import urllib2, ast
 
 @app.route('/')
 @app.route('/index')
@@ -19,3 +20,65 @@ def index():
                            title='Home',
                            user=user,
                            recipes=recipes)
+
+API_KEY = "8ded784ee30fccebd50438b3e1db5866"
+BASE_SEARCH = "http://food2fork.com/api/search?key="
+BASE_GET = "http://food2fork.com/api/get?key="
+
+BASE_RESULT = {"Name" : "",
+               "URL" : "",
+               "Ingredients" : "",
+               "Image_Url" : ""}
+
+def numIngredients(ingredients):
+    if (ingredients == ""):
+        return 0
+    ingredients_list = ingredients.split(",")
+    return len(ingredients_list)
+
+def removeIngredient(ingredients):
+    ingredients_list = ingredients.split(",")
+    if (ingredients_list[0] == ''): # No ingredients
+        return ingredients
+    else:
+        ingredients_list.pop(0)
+        return ','.join(ingredients_list)
+
+@app.route("/user/<ingredients>")
+# @app.route("/user/<string:ingredients>/")
+def getRecipe(ingredients):
+    if (ingredients == ""):
+        return str(BASE_RESULT)
+    searchRequest = BASE_SEARCH + API_KEY
+    ingredients = "&q=" + ingredients
+    searchRequest = searchRequest + ingredients
+    try:
+        content = urllib2.urlopen(searchRequest).read()
+        contentDict = ast.literal_eval(content)
+        count = contentDict["count"]
+
+        while ((count == 0) and (numIngredients(ingredients) > 1)):
+            ingredients = removeIngredient(ingredients)
+            content = urllib2.urlopen(BASE_SEARCH + API_KEY + "&q=" + ingredients).read()
+            contentDict = ast.literal_eval(content)
+            count = contentDict["count"]
+
+        if (count == 0):
+            return str(BASE_RESULT)
+        else:
+            finalDict = contentDict["recipes"][0]
+            searchGet = BASE_GET + API_KEY + "&rId=" + finalDict["recipe_id"]
+            finalContentDictStr = urllib2.urlopen(searchGet).read()
+            finalContentDict = ast.literal_eval(finalContentDictStr)
+            completeDict = {"Name" : "",
+                            "URL" : "",
+                            "Ingredients" : "",
+                            "Image_Url" : ""}
+            completeDict["Name"] = finalContentDict["recipe"]["title"]
+            completeDict["URL"] = finalContentDict["recipe"]["source_url"]
+            completeDict["Ingredients"] = str(finalContentDict["recipe"]["ingredients"])
+            completeDict["Image_Url"] = finalContentDict["recipe"]["image_url"]
+            return str(completeDict)
+    except:
+        return str(BASE_RESULT)
+
